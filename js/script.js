196139c40c2737e2cdcb5c5817d4314747512b3a
aidenraadh <aidenraadh@gmail.com>
2022-08-30 21:55:31 +0700

    --

--- js/script.js
+++ js/script.js
@@ -3,30 +3,43 @@ gsap.registerPlugin(ScrollTrigger)
 let container = document.getElementById("container");
 
 gsap.to(container, {
-  x: () => -(container.scrollWidth - document.documentElement.clientWidth - 100) + "px",
-  ease: "none",
-  scrollTrigger: {
-    trigger: container,
-    invalidateOnRefresh: true,
-    pin: true,
-    scrub: 1,
-    end: () => "+=" + container.offsetWidth
-  }
+    x: () => -(container.scrollWidth - document.documentElement.clientWidth) + "px",
+    ease: "none",
+    scrollTrigger: {
+        trigger: container,
+        invalidateOnRefresh: true,
+        pin: true,
+        scrub: 1,
+        end: () => "+=" + container.offsetWidth
+    }
 })
+
+let sections = gsap.utils.toArray(".panel");
+
+let scrollTween = gsap.to(sections, {
+    xPercent: -100 * (sections.length - 1),
+    ease: "none", // <-- IMPORTANT!
+    scrollTrigger: {
+        trigger: ".bar",
+        pin: true,
+        scrub: 0.1,
+        //snap: directionalSnap(1 / (sections.length - 1)),
+        end: "+=3000"
+    }
+});
 
 // --- bar inside Purple panel ---
 gsap.from(".bar", {
     scrollTrigger: {
-      trigger: "#section-3",
-      scrub: true,
-      pin: true,
-    //   pinSpacing: true,
-      start: "center left",
-      end: "+=100%",
-    //   onEnter: () => setActive(links[1]),
-    //   onEnterBack: () => setActive(links[1]),
+        markers: true,
+        trigger: ".bar",
+        containerAnimation: scrollTween,
+        scrub: true,
+        // pin: '#section-3',
+        start: "left 80%",
+        end: "center 20%",
     },
-    scaleX: 0, 
+    scaleX: 5, 
     transformOrigin: "left center", 
     ease: "none"
-  });
+});
